d5d5f199f53318d9afd6bfcdb9d93546977afb52
Dunciusss <123763585+Dunciusss@users.noreply.github.com>
2023-03-18 13:43:51 +0200

    Flappy Bird With Sprites

--- Assets/Bird.cs
+++ Assets/Bird.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class Bird : MonoBehaviour
 {
     public Rigidbody2D rb;
-
+    public AudioSource jumpSound;
+    public AudioSource collisionSound;
 
 
     private void Update()
     {
         if (Input.anyKey) Jump();
+        
     }
     void Jump()
     {
+        jumpSound.Play();
         rb.velocity = new Vector2(0, 5);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Ground")
+        {
+            collisionSound.Play();
+        }
     }
 }
